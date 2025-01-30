10cd095de4d0f8a1f5571b87ac39e934099790fd
CryptoPenguin15 <68248498+CryptoPenguin15@users.noreply.github.com>
2025-01-30 09:00:12 +0100

    Update tests.sh

--- tests.sh
+++ tests.sh
@@ -9,7 +9,7 @@ cargo test --release -- --nocapture ::test_encrypt_block_tfhe2 -- --exact
 cargo test --release -- --nocapture ::test_decrypt_block_tfhe2 -- --exact
 cargo test --release -- --nocapture ::test_encrypt_decrypt_rnd_block -- --exact
 
-cargo run --release -- -n 1 -i "00112233445566778899aabbccddeeff" -k "0123456789abcdef0123456789abcdef"
-cargo run --release -- -n 2 -i "00112233445566778899aabbccddeeff" -k "0123456789abcdef0123456789abcdef"
+cargo run --release -- -n 1 -i "0123456789abcdef" -k "0123456789abcdef0123456789abcdef"
+cargo run --release -- -n 2 -i "0123456789abcdef" -k "0123456789abcdef0123456789abcdef"
 
 echo "READY!"
